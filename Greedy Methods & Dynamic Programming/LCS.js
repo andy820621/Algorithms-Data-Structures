8b6240e271ceb094a353de0e69c173a32b0535e5
BarZ <andy820621@gmail.com>
2022-10-13 09:24:56 +0900

    fix: can find all possible result

--- Greedy Methods & Dynamic Programming/LCS.js
+++ Greedy Methods & Dynamic Programming/LCS.js
@@ -1,63 +1,82 @@
 function LCS(str1, str2) {
 	let tableValue = [];
 	let tableFrom = [];
-	let result = "";
 
+	// build table
 	for (let i = 0; i <= str1.length; i++) {
-		// set tavleValue
+		// set tableValue
 		tableValue.push([]);
 		tableValue[i][0] = 0;
-		// set tavleFrom
+		// set tableFrom
 		tableFrom.push([]);
 		tableFrom[i][0] = null;
 		for (let j = 1; j <= str2.length; j++) {
-			// set tavleValue
+			// set tableValue
 			tableValue[i][j] = i === 0 ? 0 : null;
-			// set tavleFrom
+			// set tableFrom
 			tableFrom[i][j] = null;
 		}
 	}
 
-	// complete table
+	// complete the table
 	for (let i = 1; i <= str1.length; i++) {
 		for (let j = 1; j <= str2.length; j++) {
 			if (str1[i - 1] === str2[j - 1]) {
-				// set tavleValue
+				// set tableValue
 				tableValue[i][j] = 1 + tableValue[i - 1][j - 1];
-				// set tavleFrom
+				// set tableFrom
 				tableFrom[i][j] = "↖";
 			} else {
-				// set tavleValue
-				tableValue[i][j] =
-					tableValue[i - 1][j] >= tableValue[i][j - 1]
-						? tableValue[i - 1][j]
-						: tableValue[i][j - 1];
-				// set tavleFrom
-				tableFrom[i][j] =
-					tableValue[i - 1][j] >= tableValue[i][j - 1] ? "↑" : "←";
+				if (tableValue[i - 1][j] === tableValue[i][j - 1]) {
+					// set tableValue
+					tableValue[i][j] = tableValue[i - 1][j];
+					// set tableFrom
+					tableFrom[i][j] = ["↑", "←"];
+				} else {
+					// set tableValue
+					tableValue[i][j] =
+						tableValue[i - 1][j] > tableValue[i][j - 1]
+							? tableValue[i - 1][j]
+							: tableValue[i][j - 1];
+					// set tableFrom
+					tableFrom[i][j] =
+						tableValue[i - 1][j] > tableValue[i][j - 1] ? "↑" : "←";
+				}
 			}
 		}
 	}
 
 	// printer
-	function printer(i, j) {
-		if (i === 0 || j === 0) return "";
+	function getResult(i, j) {
+		const set = new Set();
 
-		if (tableFrom[i][j] === "↖") {
-			printer(i - 1, j - 1);
-			result += str1[i - 1];
-		} else {
-			let index_1 = tableFrom[i][j] === "↑" ? i - 1 : i;
-			let index_2 = tableFrom[i][j] === "←" ? j - 1 : j;
-			printer(index_1, index_2);
+		printer(i, j, "");
+		function printer(i, j, textStack) {
+			if (i === 0 || j === 0) return set.add(textStack);
+
+			if (tableFrom[i][j] === "↖") {
+				// textStack = textStack.replace(/^/, str1[i - 1]);
+				textStack = str1[i - 1] + textStack;
+				printer(i - 1, j - 1, textStack);
+			} else if (Array.isArray(tableFrom[i][j])) {
+				printer(i - 1, j, textStack);
+				printer(i, j - 1, textStack);
+			} else {
+				let index_1 = tableFrom[i][j] === "↑" ? i - 1 : i;
+				let index_2 = tableFrom[i][j] === "←" ? j - 1 : j;
+				printer(index_1, index_2, textStack);
+			}
 		}
+
+		return [...set];
 	}
-	printer(str1.length, str2.length);
+
+	const result = getResult(str1.length, str2.length);
 
 	console.log(result);
 	return result;
 }
 
-LCS("ABACABE", "BACCHAE"); // BACAE
-LCS("ATAACGCGCTGCTCGGGT", "TCAATCAGGATCCGCTGA"); // TAACGCCGCTG
-LCS("TCAATCAGGATCCGCTGA", "ATAACGCGCTGCTCGGGT"); // TAACGGTCCGT
+// LCS("ABACABE", "BACCHAE"); // [ 'BACAE' ]
+LCS("ATAACGCGCTGCTCGGGT", "TCAATCAGGATCCGCTGA"); // ["TAACGCCGCTG", "TAACGGCGCTG", "TAACGGTGCTG", "TAACGGTCCGG", "TAACGGTCCGT"]
+// LCS("TCAATCAGGATCCGCTGA", "ATAACGCGCTGCTCGGGT"); // ["TAACGGTCCGT", "TAACGGTCCGG", "TAACGGTGCTG", "TAACGGCGCTG", "TAACGCCGCTG"]
